7abcbc231a3df272a1653289a18e83d71c5d3650
ARPIT DADHICH <arpit.dadhich@s.amity.edu>
2023-10-08 22:11:59 +0530

    Added basic setup

---
+++ src/components/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const HomeComponent:React.FC = () => {
+  return (
+    <div className={styles.mainContainer}>
+        <div className={styles.detailsContainer}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default HomeComponent;
