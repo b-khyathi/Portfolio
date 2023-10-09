4b7444877c23281c68b8f5404f96cfb2fbe15055
ARPIT DADHICH <arpit.dadhich@s.amity.edu>
2023-10-09 23:07:07 +0530

    Added quote on homepage

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from './index.module.scss'
+import { useTranslation } from 'react-i18next'
 
 const HomeComponent: React.FC = () => {
+    const {t} = useTranslation();
     return (
         <div className={styles.mainContainer}>
             <div className={styles.detailsContainer}>
+                <div className={styles.quote}>
+                    { t('homepage.quote') }
+                </div>
             </div>
         </div>
     )
